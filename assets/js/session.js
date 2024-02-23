9cd49f9591e267f1e4291be2f4a44786d206bfff
Angel163200 <angelmariathomas163@gmail.com>
2024-02-23 17:40:47 +0530

    profile-working on

--- assets/js/session.js
+++ assets/js/session.js
@@ -1,8 +1,15 @@
-
 var userDetails = window.localStorage.getItem('expense_tracker_app_session');
 userDetails = JSON.parse(userDetails);
-document.getElementById('fullname').innerHTML= userDetails.fullname;
-document.getElementById('fname').innerHTML= userDetails.fullname;
-document.getElementById('names').innerHTML= userDetails.fullname;
-document.getElementById('profilepic').src = userDetails.profileImageUrl;  
-console.log(userDetails.fullname);
+
+console.log(userDetails)
+document.getElementById('fullname').innerHTML = userDetails.fullname;
+document.getElementById('fname').innerHTML = userDetails.fullname;
+document.getElementById('names').innerHTML = userDetails.fullname;
+
+if (userDetails.profileImgUrl) {
+    console.log('Profile Image URL:', userDetails.profileImgUrl);
+  document.getElementById('profilepic').src = userDetails.profileImgUrl;
+} 
+else {
+  document.getElementById('profilepic').src = 'assets/img/profile-img.jpg';
+}
