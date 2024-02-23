392c3967b9f8d8c6e5e1da7f96a140ec207ba685
Angel163200 <angelmariathomas163@gmail.com>
2024-02-23 13:02:08 +0530

    profile image

--- assets/js/session.js
+++ assets/js/session.js
@@ -4,4 +4,5 @@ userDetails = JSON.parse(userDetails);
 document.getElementById('fullname').innerHTML= userDetails.fullname;
 document.getElementById('fname').innerHTML= userDetails.fullname;
 document.getElementById('names').innerHTML= userDetails.fullname;
+document.getElementById('profilepic').src = userDetails.profileImageUrl;  
 console.log(userDetails.fullname);
