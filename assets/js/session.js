b88528a9875ca8bdc2c1c70c19faf7fcf4c30c9a
Angel163200 <angelmariathomas163@gmail.com>
2024-02-25 11:49:03 +0530

    signout

--- assets/js/session.js
+++ assets/js/session.js
@@ -4,7 +4,7 @@ userDetails = JSON.parse(userDetails);
 console.log(userDetails)
 document.getElementById('fullname').innerHTML = userDetails.fullname;
 document.getElementById('fname').innerHTML = userDetails.fullname;
-document.getElementById('names').innerHTML = userDetails.fullname;
+// document.getElementById('names').innerHTML = userDetails.fullname;
 
 if (userDetails.profileImgUrl) {
     console.log('Profile Image URL:', userDetails.profileImgUrl);
